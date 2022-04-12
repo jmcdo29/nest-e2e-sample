974ed946a6786f514d68757b2bb7fcf9b226b4f6
Jay McDoniel <jmcdo29@gmail.com>
2022-04-12 15:31:48 -0700

    feat: add in ogma as a logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,32 @@ import { NeighborhoodModule } from './neighborhood/neighborhood.module';
 import { KyselyModule } from './kysely/kysely.module';
 import { FamilyModule } from './family/family.module';
 import { PersonModule } from './person/person.module';
+import { OgmaInterceptor, OgmaModule } from '@ogma/nestjs-module';
+import { ExpressParser } from '@ogma/platform-express';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [NeighborhoodModule, KyselyModule, FamilyModule, PersonModule],
+  imports: [
+    OgmaModule.forRoot({
+      service: {
+        application: 'E2E Sample',
+      },
+      interceptor: {
+        http: ExpressParser,
+      },
+    }),
+    NeighborhoodModule,
+    KyselyModule,
+    FamilyModule,
+    PersonModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: OgmaInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,6 +3,7 @@ import './templates';
 
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { Ogma } from '@ogma/logger';
 import { exec as cbExec } from 'child_process';
 import { request } from 'pactum';
 import { join } from 'path';
@@ -13,15 +14,19 @@ import { AppModule } from '../src/app.module';
 import { getKyselyOptionsToken } from '../src/kysely';
 import { parse } from 'uvu/parse';
 
+const ogma = new Ogma({ application: 'E2E Sample Tests', logLevel: 'ALL' });
+
 const exec = promisify(cbExec);
 
 const runPostgres = async () => {
+  ogma.debug('Checking if Postgres is running');
   const dockerStatus = JSON.parse(
     (await exec('docker compose -p test ps --format json')).stdout.toString(),
   );
   if (dockerStatus[0]?.State === 'running') {
     return;
   }
+  ogma.debug('Postgres is not running. Starting via docker compose');
   await exec('docker compose -p test -f docker-compose.test.yml up -d');
   let exitCode = 1;
   do {
@@ -33,17 +38,20 @@ const runPostgres = async () => {
       exitCode = 1;
     }
   } while (exitCode !== 0);
+  ogma.debug('netcat returned code 0. Finalizing wait time');
   await new Promise((resolve) => setTimeout(resolve, 1000));
 };
 
 const runMigrations = async () => {
+  ogma.debug('Running Migrations');
   const { stdout } = await exec(
     'NODE_ENV=test node -r @swc/register db/index.ts',
   );
-  console.log(stdout);
+  ogma.debug(stdout);
 };
 
 const setupApplication = async (): Promise<INestApplication> => {
+  ogma.debug('Creating Nest application');
   const modRef = await Test.createTestingModule({
     imports: [AppModule],
   })
@@ -74,11 +82,15 @@ const runTests = async () => {
   await runPostgres();
   await runMigrations();
   const app = await setupApplication();
+  ogma.debug('Starting Nest application');
   await app.listen(0);
   const url = await app.getUrl();
   request.setBaseUrl(url.replace('[::1]', 'localhost'));
+  ogma.debug('Running uvu programatically');
   await runUvu();
+  ogma.debug('Closing Nest Application');
   await app.close();
+  ogma.debug('E2E tests finished');
 };
 
 runTests();
